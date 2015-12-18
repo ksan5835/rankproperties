fe7c348cb2c3f3dcf4726985218ddcab66ca2f6f
sankar <shanmuga.sankar@samrantech.com>
2015-12-18 10:57:09 +0530

    rank prop issue fixed

--- wp-content/themes/rankproperties/header-banner.php
+++ wp-content/themes/rankproperties/header-banner.php
@@ -28,11 +28,19 @@
 	</div><!--.main-owl-carousel-->
     </div><!--.home-carousel-->
 <?php else: ?>
+
+<?php $featuredimageurl = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
+
+
     <div id="banner">
-    <?php if(of_get_option('default_banner_image')): ?>
+    <?php if($featuredimageurl): ?>
+	<img src="<?php echo$featuredimageurl; ?>" alt="<?php bloginfo( 'name' ); ?>-Banner" class="headerbanner">
+    <?php elseif(of_get_option('default_banner_image')): ?>
 	<img src="<?php echo of_get_option('default_banner_image'); ?>" alt="<?php bloginfo( 'name' ); ?>-Banner">
     <?php else: ?>
 	<img src="<?php echo get_template_directory_uri() . '/includes/images/banner.jpg'?>" alt="<?php bloginfo( 'name' ); ?>-Banner">
     <?php endif; ?>
+    
+    
     </div>
 <?php endif; ?>
